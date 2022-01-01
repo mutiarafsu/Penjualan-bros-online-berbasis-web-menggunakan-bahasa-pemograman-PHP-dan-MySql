3454afd5e6f5b25a17932bfec4ff66f752fa595a
MUTIARA FRENDA SARI UTAMI <96958364+mutiarafsu@users.noreply.github.com>
2022-01-02 05:13:54 +0700

    Create pesan.php

---
+++ pesan.php
@@ -0,0 +1,9 @@
+<div id="formadd">
+<fieldset>
+<h3>PANDUAN PEMESANAN</h3>
+<form action="" method="get">
+  <p>Cara pesan :</p>
+  <p>1. Jika produk yang dibeli melebihi dari stok yang tersedia maka anda dapat menekan tombol <strong>Pesan. </strong>Tunggu konfirmasi dari admin untuk tanggal terpenuhinya stok produk dan tanggal pengiriman barang. Untuk konfirmasi stok produk admin akan mengirim email kepada anda atau anda dapat mengunjungi situs kami. Jika anda setuju dengan tanggal yang telah kami konfirmasi ka silahkan melakukan pembayaran</p>
+  <p>2. Untuk pemesanan dalam jumlah banyak silahkan isikan Nama Barang, Jumlah Barang dan Pilihan Warna <u>atau</u> isi &quot;Lengkap&quot; pada kolom warna jika ingin memiliki semua pilihan warna</p>
+  <p>3. Syarat dan ketentuan berlaku</p>
+</form>
