dc490f693987d5f0ea9579bb768b77ceb0156303
MUTIARA FRENDA SARI UTAMI <96958364+mutiarafsu@users.noreply.github.com>
2022-01-02 05:04:48 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+<div class="grid">
+    <div class="dh3">
+	  <p>&nbsp;</p>
+	  <p>&nbsp;</p>
+    </div>
+<div class="dh3">
+		<p><b><span style="color:#333333; font-size:18px;"> Layanan Pelanggan</span></b></p>
+		<p>
+		<a href="?p=pesan"><b><span style="color:#FFFFFF; font-size:14px;"> - Penduan Pemesanan</span></b></a><br>
+		<a href="#"><b><span style="color:#FFFFFF; font-size:14px;"> - Syarat dan Kondisi</span></b></a><br>
+		<a href="#"><b><span style="color:#FFFFFF; font-size:14px;"> - Kebijakan Privasi</span></b></a><br>
+	    <a href="#"><b><span style="color:#FFFFFF; font-size:14px;"> - Bantuan</span></b></a>
+  </div>
+<div class="dh3">
+		<p><b><span style="color:#333333; font-size:18px;"> Media Sosial</span></b></p>
+  <p>
+		<a href=""><img src="icon/in1.jpg"></a>
+		<b><span style="color:#FFFFFF; font-size:16px;"> alifhandmade </span></b> <a href=""><img src="icon/in2.jpg"></a><b><span style="color:#FFFFFF; font-size:16px;"> @alif_handmade </span></b> 
+	    <p>
+	</div>
+	<div class="dh3">
+	  <p>&nbsp;</p>
+    </div>
+  </div>
+  <p align="center"><b><span style="color:#333333; font-size:14px;">Copyright &#169; 
+  2017 Wilna Maulida 1408 | Padang</span></b></p>
+<p align="center">&nbsp;</p>
